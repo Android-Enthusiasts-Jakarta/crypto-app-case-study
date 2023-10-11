a516be3ff2b1309c8d942043e8f36d658487ccc1
Syntax <fiqrihafzainislami.com>
2023-10-11 19:52:55 +0700

    Create `CryptoFeed` model

---
+++ app/src/main/java/CryptoFeed.kt
@@ -0,0 +1,19 @@
+data class CryptoFeed(
+    val coinInfo: CoinInfo,
+    val raw: Raw
+)
+
+data class CoinInfo(
+    val id: String,
+    val name: String,
+    val fullName: String
+)
+
+data class Raw(
+    val usd: Usd
+)
+
+data class Usd(
+    val price: Double,
+    val changePctDay: Long
+)
