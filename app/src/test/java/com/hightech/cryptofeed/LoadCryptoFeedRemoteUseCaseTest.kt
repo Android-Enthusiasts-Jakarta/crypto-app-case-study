9bc9b52e8a7d3cb4dc4bf69feeaf756dc584225c
Syntax <fiqrihafzainislami.com>
2023-10-14 15:40:34 +0700

    Guarantee that we can load more than once

--- app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -24,6 +24,16 @@ class LoadCryptoFeedRemoteUseCaseTest {
         assertEquals(1, client.getCount)
     }
 
+    @Test
+    fun testLoadTwiceRequestsDataTwice() {
+        val (sut, client) = makeSut()
+
+        sut.load()
+        sut.load()
+
+        assertEquals(2, client.getCount)
+    }
+
     private fun makeSut(): Pair<LoadCryptoFeedRemoteUseCase, HttpClientSpy> {
         val client = HttpClientSpy()
         val sut = LoadCryptoFeedRemoteUseCase(client = client)
