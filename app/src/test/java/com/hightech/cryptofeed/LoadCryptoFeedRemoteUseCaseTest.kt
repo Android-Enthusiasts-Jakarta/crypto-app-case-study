32d741f0bc3a94b4541d73978971053f2bde9e23
Syntax <fiqrihafzainislami.com>
2023-10-14 13:57:24 +0700

    Move test logic to separate type `HttpClientSpy`

--- app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -6,22 +6,34 @@ import org.junit.Test
 
 class LoadCryptoFeedRemoteUseCase {
     fun load() {
-        HttpClient.instance.getCount = 1
+        HttpClient.instance.get()
     }
 }
 
-class HttpClient private constructor() {
+open class HttpClient {
     companion object {
-        val instance = HttpClient()
+        var instance = HttpClient()
     }
+
+    open fun get() {
+
+    }
+}
+
+class HttpClientSpy: HttpClient() {
     var getCount = 0
+
+    override fun get() {
+        getCount += 1
+    }
 }
 
 class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient.instance
+        val client = HttpClientSpy()
+        HttpClient.instance = client
         LoadCryptoFeedRemoteUseCase()
 
         assertTrue(client.getCount == 0)
@@ -29,7 +41,8 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
     fun testLoadRequestData() {
-        val client = HttpClient.instance
+        val client = HttpClientSpy()
+        HttpClient.instance = client
         val sut = LoadCryptoFeedRemoteUseCase()
 
         sut.load()
