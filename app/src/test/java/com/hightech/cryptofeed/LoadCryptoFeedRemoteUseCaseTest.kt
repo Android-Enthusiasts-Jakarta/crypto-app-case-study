0d359527b3b60ccebf1f09076b29a96a1a1f306a
Syntax <fiqrihafzainislami.com>
2023-10-14 13:37:34 +0700

    Request data once on load

--- app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,22 +1,39 @@
 package com.hightech.cryptofeed
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class LoadCryptoFeedRemoteUseCase {
-
+    fun load() {
+        HttpClient.instance.getCount = 1
+    }
 }
 
-class HttpClient {
+class HttpClient private constructor() {
+    companion object {
+        var instance = HttpClient()
+    }
     var getCount = 0
 }
 
 class LoadCryptoFeedRemoteUseCaseTest {
+
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient()
+        val client = HttpClient.instance
         LoadCryptoFeedRemoteUseCase()
 
         assertTrue(client.getCount == 0)
+    }
+
+    @Test
+    fun testLoadRequestData() {
+        val client = HttpClient.instance
+        val sut = LoadCryptoFeedRemoteUseCase()
+
+        sut.load()
+
+        assertEquals(1, client.getCount)
     }
 }
