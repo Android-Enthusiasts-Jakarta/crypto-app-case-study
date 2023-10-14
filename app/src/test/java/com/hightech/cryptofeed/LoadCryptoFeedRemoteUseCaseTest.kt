c106790cddb8e3246a773ee2d69cc5fa02d9e210
Syntax <fiqrihafzainislami.com>
2023-10-14 14:31:07 +0700

    Refactor test to remove duplication

--- app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -16,31 +16,35 @@ interface HttpClient {
     fun get()
 }
 
-class HttpClientSpy: HttpClient {
-    var getCount = 0
-
-    override fun get() {
-        getCount += 1
-    }
-}
-
 class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClientSpy()
-        LoadCryptoFeedRemoteUseCase(client = client)
+        val (_, client) = makeSut()
 
         assertTrue(client.getCount == 0)
     }
 
     @Test
     fun testLoadRequestData() {
-        val client = HttpClientSpy()
-        val sut = LoadCryptoFeedRemoteUseCase(client = client)
+        val (sut, client) = makeSut()
 
         sut.load()
 
         assertEquals(1, client.getCount)
+    }
+
+    private fun makeSut(): Pair<LoadCryptoFeedRemoteUseCase, HttpClientSpy> {
+        val client = HttpClientSpy()
+        val sut = LoadCryptoFeedRemoteUseCase(client = client)
+        return Pair(sut, client)
+    }
+
+    private class HttpClientSpy: HttpClient {
+        var getCount = 0
+
+        override fun get() {
+            getCount += 1
+        }
     }
 }
