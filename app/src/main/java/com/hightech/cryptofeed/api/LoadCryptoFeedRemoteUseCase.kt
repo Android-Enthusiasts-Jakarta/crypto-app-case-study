bb4e98e22ce75b85b5c7e25365880c39932108a7
Syntax <fiqrihafzainislami.com>
2023-10-29 09:40:47 +0700

    Guarantee we only receive one error

--- app/src/main/java/com/hightech/cryptofeed/api/LoadCryptoFeedRemoteUseCase.kt
+++ app/src/main/java/com/hightech/cryptofeed/api/LoadCryptoFeedRemoteUseCase.kt
@@ -1,13 +1,20 @@
 package com.hightech.cryptofeed.api
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 interface HttpClient {
-    fun get()
+    fun get(): Flow<Exception>
 }
 
 class LoadCryptoFeedRemoteUseCase constructor(
     private val client: HttpClient
 ) {
-    fun load() {
-        client.get()
+    fun load(): Flow<Exception> = flow {
+        client.get().collect { error ->
+            emit(Connectivity())
+        }
     }
 }
+
+class Connectivity: Exception()
--- app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/hightech/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,7 +1,11 @@
 package com.hightech.cryptofeed
 
+import com.hightech.cryptofeed.api.Connectivity
 import com.hightech.cryptofeed.api.HttpClient
 import com.hightech.cryptofeed.api.LoadCryptoFeedRemoteUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -34,6 +38,20 @@ class LoadCryptoFeedRemoteUseCaseTest {
         assertEquals(2, client.getCount)
     }
 
+    @Test
+    fun testLoadDeliversErrorOnClientError() = runBlocking {
+        val (sut, client) = makeSut()
+
+        val capturedError = arrayListOf<Exception>()
+        sut.load().collect { error ->
+            capturedError.add(error)
+        }
+
+        client.error = Exception("Test")
+
+        assertEquals(listOf(Connectivity::class.java), capturedError.map { it::class.java })
+    }
+
     private fun makeSut(): Pair<LoadCryptoFeedRemoteUseCase, HttpClientSpy> {
         val client = HttpClientSpy()
         val sut = LoadCryptoFeedRemoteUseCase(client = client)
@@ -42,8 +60,12 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
     private class HttpClientSpy: HttpClient {
         var getCount = 0
+        var error: Exception? = null
 
-        override fun get() {
+        override fun get(): Flow<Exception> = flow {
+            if (error != null) {
+                emit(error ?: Exception())
+            }
             getCount += 1
         }
     }
