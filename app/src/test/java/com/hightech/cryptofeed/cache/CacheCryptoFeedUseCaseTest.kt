de1a1ae56ecf56907056b5d32e87814fe51af564
Syntax <fiqrihafzainislami.com>
2023-11-25 10:04:40 +0700

    Save command requests cache deletion

--- app/src/test/java/com/hightech/cryptofeed/cache/CacheCryptoFeedUseCaseTest.kt
+++ app/src/test/java/com/hightech/cryptofeed/cache/CacheCryptoFeedUseCaseTest.kt
@@ -1,18 +1,23 @@
 package com.hightech.cryptofeed.cache
 
-import com.hightech.cryptofeed.api.LoadCryptoFeedRemoteUseCase
-import io.mockk.MockKAnnotations
+import com.hightech.cryptofeed.domain.CoinInfo
+import com.hightech.cryptofeed.domain.CryptoFeed
+import com.hightech.cryptofeed.domain.Raw
+import com.hightech.cryptofeed.domain.Usd
 import io.mockk.confirmVerified
 import io.mockk.spyk
 import io.mockk.verify
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import java.util.UUID
 
 class CacheCryptoFeedUseCase constructor(
     private val store: RoomCryptoFeedStore
 ) {
-
+    fun save(feeds: List<CryptoFeed>) {
+        store.deleteCache()
+    }
 }
 
 class RoomCryptoFeedStore {
@@ -38,4 +43,34 @@ class CacheCryptoFeedUseCaseTest {
 
         confirmVerified(store)
     }
-}
+
+    @Test
+    fun testSaveRequestsCacheDeletion() = runBlocking {
+        val feeds = listOf(uniqueCryptoFeed(), uniqueCryptoFeed())
+
+        sut.save(feeds)
+
+        verify(exactly = 1) {
+            store.deleteCache()
+        }
+
+        confirmVerified(store)
+    }
+
+    private fun uniqueCryptoFeed(): CryptoFeed {
+        return CryptoFeed(
+            CoinInfo(
+                UUID.randomUUID().toString(),
+                "any",
+                "any",
+                "any-url"
+            ),
+            Raw(
+                Usd(
+                    1.0,
+                    1F,
+                )
+            )
+        )
+    }
+ }
