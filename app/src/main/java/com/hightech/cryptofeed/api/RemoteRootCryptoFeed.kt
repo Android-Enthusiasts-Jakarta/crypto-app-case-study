91432e070afa56228bc49a4d8565decad7f27ba4
Syntax <fiqrihafzainislami.com>
2024-04-21 12:01:41 +0700

    Move `CryptoFeedResponse` components to it's own file

--- app/src/main/java/com/hightech/cryptofeed/api/RemoteRootCryptoFeed.kt
+++ app/src/main/java/com/hightech/cryptofeed/api/RemoteRootCryptoFeed.kt
@@ -1,42 +1,5 @@
 package com.hightech.cryptofeed.api
 
-import com.squareup.moshi.Json
-
-data class RootCryptoFeedResponse(
-    @Json(name = "Data")
-    val data: List<CryptoFeedResponse>
-)
-
-data class CryptoFeedResponse(
-    @Json(name = "CoinInfo")
-    val coinInfoResponse: CoinInfoResponse,
-    @Json(name = "RAW")
-    val rawResponse: RawResponse
-)
-
-data class CoinInfoResponse(
-    @Json(name = "Id")
-    val id: String,
-    @Json(name = "Name")
-    val name: String,
-    @Json(name = "FullName")
-    val fullName: String,
-    @Json(name = "ImageUrl")
-    val imageUrl: String,
-)
-
-data class RawResponse(
-    @Json(name = "USD")
-    val usdResponse: UsdResponse
-)
-
-data class UsdResponse(
-    @Json(name = "PRICE")
-    val price: Double,
-    @Json(name = "CHANGEPCTDAY")
-    val changePctDay: Float
-)
-
 data class RemoteRootCryptoFeed(
     val data: List<RemoteCryptoFeed>
 )
